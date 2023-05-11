7354841fafb2b37aaf884df630cd62ab8b6862a7
WallisonLima <wallisonlimaoliveira@gmail.com>
2023-05-11 20:10:59 -0300

    console for teste with bot take

--- controllers/BotController.js
+++ controllers/BotController.js
@@ -8,45 +8,46 @@ class BotForm {
 
         // await db.insertDB('users', req.body)
         console.log('Iniciando')
+        console.log(req.body)
 
-        let resp = await validToken(req.headers.authorization)
-        if (resp) {
-            res.send(resp);
-            return;
-        }
+        // let resp = await validToken(req.headers.authorization)
+        // if (resp) {
+        //     res.send(resp);
+        //     return;
+        // }
 
-        resp = await validReq(req.body)
-        if (resp) {
-            res.send(resp)
-            return;
-        }
+        // resp = await validReq(req.body)
+        // if (resp) {
+        //     res.send(resp)
+        //     return;
+        // }
 
 
-        const dados = {
-            nome: req.body.nome,
-            sobreNome: req.body.sobreNome,
-            email: req.body.email,
-            cargo: req.body.cargo,
-            departamento: req.body.departamento,
-            empresa: req.body.empresa,
-            siteEmpresa: req.body.siteEmpresa
-        }
+        // const dados = {
+        //     nome: req.body.nome,
+        //     sobreNome: req.body.sobreNome,
+        //     email: req.body.email,
+        //     cargo: req.body.cargo,
+        //     departamento: req.body.departamento,
+        //     empresa: req.body.empresa,
+        //     siteEmpresa: req.body.siteEmpresa
+        // }
 
-        let { page, browser } = await botService.launch();
-        if (!page) {
-            res.send(await help.messageError(500, 'Browser default', ''))
-            return;
-        }
+        // let { page, browser } = await botService.launch();
+        // if (!page) {
+        //     res.send(await help.messageError(500, 'Browser default', ''))
+        //     return;
+        // }
 
-        page = await botService.insereDadosParceiro(page);
+        // page = await botService.insereDadosParceiro(page);
 
-        page = await botService.insereDadosIndicado(page, dados);
+        // page = await botService.insereDadosIndicado(page, dados);
 
-        page = await botService.selecionaDadosIndicado(page, dados);
+        // page = await botService.selecionaDadosIndicado(page, dados);
 
-        page = await botService.clickBoxIndicado(page, dados);
+        // page = await botService.clickBoxIndicado(page, dados);
 
-        browser.disconnect();
+        // browser.disconnect();
 
         console.log("Processo finalizado")
         await res.send(help.messageError(200, 'Process concluded!', ''))
