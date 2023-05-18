95918138bb7c7190ceeee9227a3ab2b98af7bd3e
WallisonLima <wallisonlimaoliveira@gmail.com>
2023-05-18 13:19:38 -0300

    bot ok

--- controllers/BotController.js
+++ controllers/BotController.js
@@ -34,8 +34,6 @@ class BotForm {
             siteEmpresa: req.body.siteEmpresa
         }
 
-        console.log(dados)
-
         let { page, browser } = await botService.launch();
         if (!page) {
             res.send(await help.messageError(500, 'Browser default', ''))
@@ -50,7 +48,7 @@ class BotForm {
 
         page = await botService.clickBoxIndicado(page, dados);
         
-        // await clickByAttr(page, "type", "submit")
+        await clickByAttr(page, "type", "submit")
 
         await browser.disconnect();
 
