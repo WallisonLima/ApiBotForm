8d4a78d63c0df0f50034d03a6b5113b874ec82c3
WallisonLima <wallisonlimaoliveira@gmail.com>
2023-05-23 11:57:58 -0300

    file vercel updated routes again

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs')
 const { PORT } = require('./constants');
+const BotController = require('./controllers/BotController');
+
 
 
 const app = express();
-const routes = require('./routes.js')
+// const routes = require('./routes.js')
 
 app.disable('x-powered-by')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(routes)
+app.post("/api/botForm/", BotController.post)
 
 app.listen(PORT, () => {
     console.log(`Example app listening at :${PORT}`)
