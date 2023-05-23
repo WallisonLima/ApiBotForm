cb402484429ee1412a972b5414fee73bac25611d
WallisonLima <wallisonlimaoliveira@gmail.com>
2023-05-23 12:06:51 -0300

    add teste in route

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs')
 const { PORT } = require('./constants');
-const BotController = require('./controllers/BotController');
+// const BotController = require('./controllers/BotController');
 
 
 
 const app = express();
-// const routes = require('./routes.js')
+const routes = require('./routes.js')
 
 app.disable('x-powered-by')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.post("/api/botForm/", BotController.post)
+// app.post("/api/botForm/", BotController.post)
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log(`Example app listening at :${PORT}`)
